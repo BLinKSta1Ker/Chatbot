fe68665aa0db9c2df159f089f8730d8d6b8b6be3
BLinKSta1Ker <siddhantpanwar@outlook.com>
2025-03-11 16:32:28 +0530

    Added JWT for initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from app.routes import chat_bp
+from app.auth import auth_bp
+from app.config import Config
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(chat_bp)
+    app.config["JWT_SECRET_KEY"] = Config.JWT_SECRET_KEY
+
+    jwt = JWTManager(app)  # Initialize JWT
+
+    app.register_blueprint(auth_bp)  # Register Auth Routes
+    app.register_blueprint(chat_bp)  # Register Chat Routes
+
     return app
