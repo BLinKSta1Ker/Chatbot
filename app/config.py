c3a31a516fdbb2cb9323523cc51b787d8ca2955e
BLinKSta1Ker <siddhantpanwar@outlook.com>
2025-03-11 16:34:38 +0530

    Added JWT key

--- app/config.py
+++ app/config.py
@@ -9,3 +9,4 @@ class Config:
     MONGO_URI = os.getenv("MONGO_URI")
     REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
     REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "supersecret")
